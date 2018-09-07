8b15c46b9678cd0a7d451ea44f1cbbf1c76a8a16
danielecarbe <daniele.carbe@gmail.com>
2018-09-07 11:45:23 +0200

    Add HTML5 page dynamic

--- MyCinema-html5/public_html/code.js
+++ MyCinema-html5/public_html/code.js
@@ -7,17 +7,40 @@
 
 
 var code = {
-    init: function() {
-    $.get("http://localhost:9090/MyCinema-server/films")
-        .done(code.onGetSuccesYeah)
-        .fail(code.onErrorBoooh);
-},
-onGetSuccesYeah : function(data) {
-    console.log(data);
-},
-onErrorBoooh: function() {
-    console.log(":....(");
-}
-};
+    init: function () {
+        $.get("http://localhost:9090/MyCinema-server/films")
+                .done(code.onGetSuccesYeah)
+                .fail(code.onErrorBoooh);
+    },
+    onGetSuccesYeah: function (data) {
+        console.log(data);
+        code.writeFilmList(data.films);
+    },
+    onErrorBoooh: function () {
+        console.log(":....(");
+    }
+    ,
+    writeFilmList: function (filmList) {
+        for (var f of filmList) {
+            var htmlStr = `<div class="cinema-panel-item">
+                <img class="film-template" src="images/MissionImpossible.jpg" alt="film template">
+                <div class="film-description">
+                    <p><strong>` + f.Title + `</strong></p>
+                    <p>
+                        <span><strong>Genere:</strong>` + f.Genere + `</span><br>
+                        <span><strong>Cast:</strong>` + f.Cast + `</span><br>
+                        <span><strong>Durata:</strong>` + f.Durata + `</span><br>
+                        <span><strong>Data Uscita:</strong>` + f.dataUscita + `</span><br></p>
+                    <p><span>Data: 04-09-2018</span><br>
+                        <span>Sala: 5      Posti: 190</span><br>
+                        <span>19:00    21:00   23:00</span>
+                    </p>
+                </div>
+            </div>`;
+            $("#films-list").append(htmlStr);
+        }
 
+    }
+
+};
 $(document).ready(code.init);
